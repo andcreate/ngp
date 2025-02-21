c9c325147e774106ef59ab507a27eb6ee99f14ca
EisukeAkimoto <ml@andask.net>
2025-02-22 07:28:19 +0900

    modify dvh

--- app/page.tsx
+++ app/page.tsx
@@ -30,10 +30,10 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="h-screen relative">
-      <div className="flex flex-col h-full items-center  align gap-5 lg:gap-8">
+      <div className="flex flex-col h-dvh items-center  align gap-5 lg:gap-8">
         <main className="flex-1 min-h-0 ">
           <Hero />
-          <div className="relative z-10 h-full flex flex-col items-center justify-center">
+          <div className="relative z-10 h-dvh flex flex-col items-center justify-center">
             <h1 className="text-5 lg:text-8 font-caveat py-8">#andCreate</h1>
             <dl className="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-8">
               <dt className="col-span-1 self-center">
